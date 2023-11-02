e3cb569cd06289bef1cec1bc26747da1e5db1dc5
Igor <megakill123@yandex.ru>
2023-11-02 18:10:09 +0500

    create project timer

---
+++ scripts/main.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+@export var seconds = 10;
+@onready var label = $CanvasLayer/VBoxContainer/Label
+@onready var timer = $Timer
+@onready var button = $CanvasLayer/VBoxContainer/MarginContainer/Button
+
+func _ready():
+	label.text = str(seconds)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_button_pressed():
+	timer.start()
+	button.disabled = true
+	
+
+
+func _on_timer_timeout():
+	if seconds == 0:
+		timer.stop()
+		seconds = 10
+		label.text = str(seconds)
+		button.disabled = false
+		return
+	seconds -= 1
+	label.text = str(seconds)
